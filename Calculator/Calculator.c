3a855781e56e5bcf6864121cf85f3495ae706cff
AGBJayanidu <106040232+AGBJayanidu@users.noreply.github.com>
2022-08-02 15:07:56 +0530

    Add files via upload

---
+++ Calculator/Calculator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char operator;
+    double number1;
+    double number2;
+    double result;
+
+    printf("Enter an operator (+ - * /): ");
+    scanf("%c", &operator);
+
+    printf("Enter number 1: ");
+    scanf("%lf", &number1);
+
+    printf("Enter number 2: ");
+    scanf("%lf", &number2);
+
+    switch (operator)
+    {
+    case '+':
+        result = number1 + number2;
+        printf("\nresult: %lf", result);
+        break;
+    case '-':
+        result = number1 - number2;
+        printf("\nresult: %lf", result);
+        break;
+    case '*':
+        result = number1 * number2;
+        printf("\nresult: %lf", result);
+        break;
+    case '/':
+        result = number1 / number2;
+        printf("\nresult: %lf", result);
+        break;
+    default:
+        printf("%c is not valid", operator);
+        break;
+    }
+
+    return 0;
+}
